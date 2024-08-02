0781bcd8ff61c732114a300800f3ab5b40c6dbe9
Sanskar <75941858+Thakur-Sanskar-Anhal@users.noreply.github.com>
2024-08-02 18:02:24 +0530

    update: updated the create component

--- marketplace.frontend/creatorHub/src/components/createComponents/chapterbar.tsx
+++ marketplace.frontend/creatorHub/src/components/createComponents/chapterbar.tsx
@@ -1,42 +1,101 @@
 "use client";
 
-import {
-  Card,
-  Typography,
-  List,
-  ListItem,
-  ListItemPrefix,
-  ListItemSuffix,
-  Chip,
-} from "@material-tailwind/react";
-import {
-  PresentationChartBarIcon,
-  ShoppingBagIcon,
-  UserCircleIcon,
-  Cog6ToothIcon,
-  InboxIcon,
-  PowerIcon,
-} from "@heroicons/react/24/solid";
+import { useState } from "react";
+import { Card, Typography, Button, IconButton } from "@material-tailwind/react";
+import { PaperAirplaneIcon, TrashIcon } from "@heroicons/react/24/solid";
+
+interface Chapter {
+  id: number;
+  name: string;
+}
 
 export function Chapterbar() {
+  const [chapters, setChapters] = useState<Chapter[]>([]);
+
+  const addChapter = () => {
+    const newChapter: Chapter = {
+      id: chapters.length + 1,
+      name: `Chapter: ${chapters.length + 1}`,
+    };
+    setChapters([...chapters, newChapter]);
+  };
+
+  const deleteChapter = (id: number) => {
+    setChapters(chapters.filter((chapter) => chapter.id !== id));
+  };
+
   return (
     <div className="w-[350px] h-screen">
       <Card
-        className="p-4 shadow-lg rounded-none h-full w-full"
+        className="px-4 shadow-lg rounded-none h-full w-full py-8"
         placeholder={undefined}
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
       >
-        <div className="mb-2 p-4">
-          <Typography
-            variant="h5"
-            color="blue-gray"
-            placeholder={undefined}
-            onPointerEnterCapture={undefined}
-            onPointerLeaveCapture={undefined}
-          >
-            Chapters :
-          </Typography>
+        <div className="flex flex-col h-screen justify-between">
+          <div className="h-full max-h-[calc(100vh-100px)] overflow-y-auto">
+            <Typography
+              variant="h5"
+              color="blue-gray"
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              Chapters
+            </Typography>
+            {chapters.map((chapter: Chapter) => (
+              <Card
+                className="p-4 m-2"
+                key={chapter.id}
+                placeholder={undefined}
+                onPointerEnterCapture={undefined}
+                onPointerLeaveCapture={undefined}
+              >
+                <div className="flex items-center justify-between">
+                  <Typography
+                    placeholder={undefined}
+                    onPointerEnterCapture={undefined}
+                    onPointerLeaveCapture={undefined}
+                  >
+                    {chapter.name}
+                  </Typography>
+                  <IconButton
+                    onClick={() => deleteChapter(chapter.id)}
+                    placeholder={undefined}
+                    onPointerEnterCapture={undefined}
+                    onPointerLeaveCapture={undefined}
+                  >
+                    <TrashIcon className="w-5 h-5" />
+                  </IconButton>
+                </div>
+              </Card>
+            ))}
+          </div>
+          <div className="flex items-center flex-grow justify-center gap-4">
+            <Button
+              variant="outlined"
+              size="lg"
+              color="blue"
+              className="w-full"
+              onClick={addChapter}
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              Add chapter
+            </Button>
+            <IconButton
+              variant="gradient"
+              size="lg"
+              color="blue"
+              className="w-full"
+              placeholder={undefined}
+              onPointerEnterCapture={undefined}
+              onPointerLeaveCapture={undefined}
+            >
+              <PaperAirplaneIcon className="w-6 h-6" />
+            </IconButton>
+          </div>
         </div>
       </Card>
     </div>
